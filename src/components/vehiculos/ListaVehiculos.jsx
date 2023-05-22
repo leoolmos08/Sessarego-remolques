9a9493da7ecaa4aae1e979897e3c94c528d524e4
leoolmos08 <leocba95@gmail.com>
2023-05-22 14:58:30 -0300

    fix appbar mobile, fix itemdetail

--- src/components/vehiculos/ListaVehiculos.jsx
+++ src/components/vehiculos/ListaVehiculos.jsx
@@ -5,6 +5,7 @@ import {
 } from "@heroicons/react/20/solid";
 
 import { API_URL } from "../../utils/constants";
+import { Link } from "react-router-dom";
 
 export default function ListaVehiculos({ vehiclesPerPage }) {
   return (
@@ -12,35 +13,37 @@ export default function ListaVehiculos({ vehiclesPerPage }) {
       <div className="mx-auto max-w-2xl px-4 py-10 sm:px-6 sm:py-12 lg:max-w-7xl lg:px-8">
         <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 xl:gap-x-8">
           {vehiclesPerPage().map((vehicle) => (
-            <a
-              key={vehicle.id}
-              href={`vehiculos/${vehicle.id}`}
-              className="group"
-            >
-              <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:h-48 xl:w-48">
-                <img
-                  src={`${API_URL}${vehicle.images[0].url}`}
-                  alt={vehicle.title}
-                  className="h-full w-full object-cover object-center group-hover:opacity-75"
-                />
-              </div>
-              <h3 className="mt-4 text-xl font-semibold">{vehicle.title}</h3>
-              <p className="mt-1 text-lg font-medium text-green-900">
-                {vehicle.price}
-              </p>
-              <div className="flex flex-row">
-                <CalendarDaysIcon className="w-5" />
-                <p className="text-sm pl-3 font-light">{vehicle.year}</p>
-              </div>
-              <div className="flex flex-row">
-                <TruckIcon className="w-5" />
-                <p className="text-sm pl-3 font-light">{vehicle.kilometros}</p>
-              </div>
-              <div className="flex flex-row">
-                <Cog6ToothIcon className="w-5" />
-                <p className="text-sm pl-3 font-light">{vehicle.combustible}</p>
-              </div>
-            </a>
+            <div key={vehicle.id} className="group">
+              <Link to={`/vehiculo/${vehicle.id}`}>
+                <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:h-48 xl:w-48">
+                  <img
+                    src={`${API_URL}${vehicle.images[0].url}`}
+                    alt={vehicle.title}
+                    className="h-full w-full object-cover object-center group-hover:opacity-75"
+                  />
+                </div>
+                <h3 className="mt-4 text-xl font-semibold">{vehicle.title}</h3>
+                <p className="mt-1 text-lg font-medium text-green-900">
+                  {vehicle.price}
+                </p>
+                <div className="flex flex-row">
+                  <CalendarDaysIcon className="w-5" />
+                  <p className="text-sm pl-3 font-light">{vehicle.year}</p>
+                </div>
+                <div className="flex flex-row">
+                  <TruckIcon className="w-5" />
+                  <p className="text-sm pl-3 font-light">
+                    {vehicle.kilometros}
+                  </p>
+                </div>
+                <div className="flex flex-row">
+                  <Cog6ToothIcon className="w-5" />
+                  <p className="text-sm pl-3 font-light">
+                    {vehicle.combustible}
+                  </p>
+                </div>
+              </Link>
+            </div>
           ))}
         </div>
       </div>
